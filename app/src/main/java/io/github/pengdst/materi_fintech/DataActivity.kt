e8f91ef10717466bb50a06e066f7e4e824310485
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-09 16:48:45 +0700

    Inisial Project

---
+++ app/src/main/java/io/github/pengdst/materi_fintech/DataActivity.kt
@@ -0,0 +1,11 @@
+package io.github.pengdst.materi_fintech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+    }
+}
