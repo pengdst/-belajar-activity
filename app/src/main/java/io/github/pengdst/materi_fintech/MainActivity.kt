1440a6b40deb1e1a2e66d2a4fe715ff91d5660f8
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-03-23 16:03:46 +0700

    start activity dengan intent

--- app/src/main/java/io/github/pengdst/materi_fintech/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/materi_fintech/MainActivity.kt
@@ -3,8 +3,10 @@ package io.github.pengdst.materi_fintech
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonSubmit: Button
     private lateinit var etNama: EditText
     private lateinit var etNim: EditText
+    private lateinit var rgGender: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +22,29 @@ class MainActivity : AppCompatActivity() {
 
         etNama = findViewById(R.id.et_nama)
         etNim = findViewById(R.id.et_nim)
+        rgGender = findViewById(R.id.rg_gender)
         buttonSubmit = findViewById(R.id.btn_submit)
 
         buttonSubmit.setOnClickListener {
             val nama = etNama.text.toString()
-            Toast.makeText(this, nama, Toast.LENGTH_SHORT).show()
+            val nim = etNim.text.toString()
+            val gender: String = when (rgGender.checkedRadioButtonId) {
+                R.id.rb_male -> "Laki-laki"
+                R.id.rb_female -> "Perempuan"
+                else -> ""
+            }
+
+            Log.e("TAG", "onCreate() called ${rgGender.checkedRadioButtonId}")
+
+            val extras = Bundle().apply {
+                putString("nama", nama)
+                putString("nim", nim)
+                putString("gender", gender)
+            }
 
             val intentGotoData = Intent(this, DataActivity::class.java).apply {
 //                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                putExtras(extras)
             }
             startActivity(intentGotoData)
         }
